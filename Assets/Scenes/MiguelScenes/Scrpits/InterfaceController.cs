6dddf65bc23b3d59c5b47ef94ee3641d80d903e0
Mangelhl <datcom221@gmail.com>
2023-05-26 19:10:01 -0500

    Interfaz Texto

---
+++ Assets/Scenes/MiguelScenes/Scrpits/InterfaceController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InterfaceController : MonoBehaviour
+{
+
+    public float idleTime = 5f; // Tiempo de inactividad antes de ocultar la interfaz
+    public Text scrollingText; // Referencia al componente Text que mostrará el texto en movimiento
+    public bool isInterfaceVisible = true;
+    private float timeSinceLastInput = 0f;
+    private string[] textContents; // Contenido del texto en movimiento
+    private TextScrolling textScrolling; // Referencia al script de desplazamiento de texto
+
+    private void Start()
+    {
+        // Inicializa el contenido del texto en movimiento
+        textContents = new string[] { "Texto 1", "Texto 2", "Texto 3" };
+
+        // Obtener referencia al script de desplazamiento de texto
+        textScrolling = GetComponentInChildren<TextScrolling>();
+
+        // Iniciar el desplazamiento de texto
+        textScrolling.StartScrolling(textContents);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // El jugador hizo clic, muestra la interfaz y reinicia el tiempo de inactividad
+            ShowInterface();
+            timeSinceLastInput = 0f;
+        }
+        else
+        {
+            // Incrementa el tiempo de inactividad
+            timeSinceLastInput += Time.deltaTime;
+
+            if (timeSinceLastInput >= idleTime)
+            {
+                // El jugador estuvo inactivo durante el tiempo especificado, oculta la interfaz
+                HideInterface();
+            }
+        }
+    }
+
+    private void ShowInterface()
+    {
+        // Activa todos los elementos de la interfaz
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+
+        isInterfaceVisible = true;
+        textScrolling.enabled = false; // Desactivar el desplazamiento de texto
+    }
+
+    private void HideInterface()
+    {
+        // Desactiva todos los elementos de la interfaz
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+        isInterfaceVisible = false;
+        textScrolling.enabled = true; // Activar el desplazamiento de texto
+    }
+}
+
